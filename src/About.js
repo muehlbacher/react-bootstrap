168f7cf5dd9140c01f4697b11eb069443ece7e74
Dominik Muehlbacher <muehlbacher.dominik@gmail.com>
2020-04-26 14:33:27 +0200

    all files

---
+++ src/About.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const About = () => (
+        <div>
+            <h2>About</h2>
+            <p>Test teas ölaskdfj löasöldfj öalksj d</p>
+        </div>
+    )
---
+++ src/Contact.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Contact = () => (
+        <div>
+            <h2>Contact Us</h2>
+            <p>Test teas ölaskdfj löasöldfj öalksj d</p>
+        </div>
+    )
